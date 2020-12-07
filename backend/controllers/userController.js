8d9a64c87b858e28dfc8d162edb71c61bcd370c7
nva12 <nvallee.a7@gmail.com>
2020-12-07 14:17:47 +0400

    update profile endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,8 +61,6 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route  GET /api/users/profile
 // @access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  // const user = await User.findById(req.user._id);
-
   if (req.user) {
     const { user } = req;
 
@@ -78,4 +76,31 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, registerUser, getUserProfile };
+// @desc   Update user profile
+// @route  PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  if (req.user) {
+    const { user } = req;
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile };
