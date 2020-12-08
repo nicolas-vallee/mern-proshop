145b7a6ed8f1c2e7ca0d9d238e14fce39c703974
nva12 <nvallee.a7@gmail.com>
2020-12-08 17:57:25 +0400

    order details endpoint, logic, and screen

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -52,7 +52,7 @@ const PlaceOrderScreen = ({ history }) => {
   };
 
   return (
-    <div>
+    <>
       <CheckoutSteps step1 step2 step3 step4 />
       <Row>
         <Col md={8}>
@@ -151,7 +151,7 @@ const PlaceOrderScreen = ({ history }) => {
           </Card>
         </Col>
       </Row>
-    </div>
+    </>
   );
 };
 
