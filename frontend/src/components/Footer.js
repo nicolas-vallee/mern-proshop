a586d60324fcc13cffa4b8b4d8a0ec8210544c14
nva12 <nvallee.a7@gmail.com>
2020-12-13 23:34:13 +0400

    customize footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,8 +5,29 @@ const Footer = () => {
   return (
     <footer>
       <Container>
-        <Row>
-          <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+        <Row className='d-flex align-items-center'>
+          <Col className='py-3'>
+            &copy; <a href='https://www.nicolasvallee.dev'>Nicolas Vallée</a>
+          </Col>
+          <Col className='py-3 text-right'>
+            <div
+              className='d-flex justify-content-end'
+              style={{ fontSize: '1.125rem' }}
+            >
+              <a
+                href='https://www.linkedin.com/in/nvallee/'
+                className='d-block ml-3'
+              >
+                <i class='fab fa-linkedin-in'></i>
+              </a>
+              <a
+                href='https://github.com/nva12/mern-proshop'
+                className='d-block ml-3'
+              >
+                <i class='fab fa-github'></i>
+              </a>
+            </div>
+          </Col>
         </Row>
       </Container>
     </footer>
